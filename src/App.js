b9ca4d12bb132620722701e604992fcc2122b18f
Emmanuel Olannye <eeolannye@gmail.com>
2026-06-18 21:24:16 -0500

    Made further changes to the component files and page files.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import { Route, Routes } from 'react-router-dom';
+import { useState } from "react";
 import Home from './pages/Home';
 import Error404 from './pages/Error404';
 import Product from './pages/Product';
 import Cart from './pages/Cart';
+import ProductSection from "./components/ProductSection";
+
+
 
 // function App() {
 //   return (
@@ -27,17 +31,21 @@ import Cart from './pages/Cart';
 // export default App;
 
 
-import { useState } from "react";
-import { Routes, Route } from "react-router-dom";
 
-import ProductSection from "./components/ProductSection";
-import Product from "./pages/Product";
+
+
 
 function App() {
   const [selectedProduct, setSelectedProduct] = useState(null);
 
   return (
     <Routes>
+                   <Route exact path='/northstar-warehouse' element={<Home />}/>
+           <Route path="/product/:id" element={<Product />} />
+
+            <Route path='*' element={<Error404 />}/>
+
+           <Route path="/cart" element={<Cart />} />
       <Route
         path="/"
         element={
--- src/Components/ProductSection.js
+++ src/Components/ProductSection.js
@@ -1,4 +1,5 @@
 import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
 import axios from "axios";
 import ProductCard from "./ProductCard";
 
@@ -52,10 +53,8 @@ import ProductCard from "./ProductCard";
 // export default ProductSection;
 
 
-import { Link } from "react-router-dom";
-import { useEffect, useState } from "react";
-import axios from "axios";
-import ProductCard from "./ProductCard";
+
+
 
 function ProductSection({ setSelectedProduct }) {
   const [products, setProducts] = useState([]);
